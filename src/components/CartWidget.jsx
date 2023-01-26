9e71f01273795c2ff6484e8afe7b34d1ef3ed50d
Leandro Soler <leandrosoler.7@gmail.com>
2023-01-26 18:43:27 -0300

    nada

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CartWidget = () => {
   return (
     <>
-        <span class="material-symbols-outlined">shopping_cart</span>
+        <span className="material-symbols-outlined">shopping_cart</span>
     </>
   );
 };
